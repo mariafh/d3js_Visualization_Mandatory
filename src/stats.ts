fa3d14700dede398a5e98aa7cba6fba82890e463
mariafh <mariafernandezhijano@gmail.com>
2020-03-24 23:49:24 +0100

    Mapa Coronavirus España v1

---
+++ src/stats.ts
@@ -0,0 +1,146 @@
+export interface ResultEntry {
+  name: string;
+  value: number;
+}
+
+export const initial_stats : ResultEntry[] =[
+  {
+    name: "Madrid",
+    value: 587
+  },
+  {
+    name: "La Rioja",
+    value: 102
+  },
+  {
+    name: "Andalucía",
+    value: 54
+  },
+  {
+    name: "Cataluña",
+    value: 101
+  },
+  {
+    name: "Valencia",
+    value: 50
+  },
+  {
+    name: "Murcia",
+    value: 5
+  },
+  {
+    name: "Extremadura",
+    value: 7
+  },
+  {
+    name: "Castilla La Mancha",
+    value: 26
+  },
+  {
+    name: "País Vasco",
+    value: 148
+  },
+  {
+    name: "Cantabria",
+    value: 12
+  },
+  {
+    name: "Asturias",
+    value: 10
+  },
+  {
+    name: "Galicia",
+    value: 18
+  },
+  {
+    name: "Aragón",
+    value: 32
+  },
+  {
+    name: "Castilla y León",
+    value: 40
+  },
+  {
+    name: "Islas Canarias",
+    value: 24
+  },
+  {
+    name: "Islas Baleares",
+    value: 11
+  },
+  {
+    name: "Navarra",
+    value: 13
+  }
+];
+
+export const final_stats : ResultEntry[] =[
+  {
+    name: "Madrid",
+    value: 12352
+  },
+  {
+    name: "La Rioja",
+    value: 802
+  },
+  {
+    name: "Andalucía",
+    value: 2471
+  },
+  {
+    name: "Cataluña",
+    value: 7864
+  },
+  {
+    name: "Valencia",
+    value: 2167
+  },
+  {
+    name: "Murcia",
+    value: 385
+  },
+  {
+    name: "Extremadura",
+    value: 636
+  },
+  {
+    name: "Castilla La Mancha",
+    value: 2465
+  },
+  {
+    name: "País Vasco",
+    value: 2728
+  },
+  {
+    name: "Cantabria",
+    value: 425
+  },
+  {
+    name: "Asturias",
+    value: 662
+  },
+  {
+    name: "Galicia",
+    value: 1415
+  },
+  {
+    name: "Aragón",
+    value: 758
+  },
+  {
+    name: "Castilla y León",
+    value: 2460
+  },
+  {
+    name: "Islas Canarias",
+    value: 557
+  },
+  {
+    name: "Islas Baleares",
+    value: 478
+  },
+  {
+    name: "Navarra",
+    value: 1014
+  }
+];
